21d29eef211f4dc7f493844f4d87161e58237779
lautaropastorino <lautaro.pastorino@hotmail.com>
2020-07-27 19:54:11 -0300

    Cambio forma de seleccion de imagenes y subo todas las escenas nuevas

--- ProyectoEntrenamientoCognitivo/Escenas/NuevoCircuito.gd
+++ ProyectoEntrenamientoCognitivo/Escenas/NuevoCircuito.gd
@@ -4,32 +4,38 @@ var colores = []
 var flechas = []
 var palabras = []
 var numeros = []
-onready var grid = $Control/imagenesGrid
+var aritmeticos = []
+var imagenes = []
 
 func _ready():
 	#Obtengo todas las imagenes posibles
-	colores = list_files_in_directory("res://Assets/ImagenesEntrenamiento/Colores")
-	flechas = list_files_in_directory("res://Assets/ImagenesEntrenamiento/Flechas")
-	palabras = list_files_in_directory("res://Assets/ImagenesEntrenamiento/PalabrasEnColores")
-	numeros = list_files_in_directory("res://Assets/ImagenesEntrenamiento/Numeros")
+	colores = list_files_in_directory("res://Escenas/Imagenes/Colores")
+	flechas = list_files_in_directory("res://Escenas/Imagenes/Flechas")
+	palabras = list_files_in_directory("res://Escenas/Imagenes/PalabrasEnColores")
+	numeros = list_files_in_directory("res://Escenas/Imagenes/Numeros")
+	aritmeticos = list_files_in_directory("res://Escenas/Imagenes/Aritmeticos")
 	
-	var imagenes_totales = {"Colores": colores, "Flechas": flechas, "PalabrasEnColores": palabras, "Numeros": numeros}
+	var imagenes_totales = {"Colores": colores, "Flechas": flechas, "Numeros": numeros}#, "PalabrasEnColores": palabras,  "Aritmeticos": aritmeticos}
 	
-	grid.set_columns(6)
-	
+	var posicion = Vector2(80, 150)
+
 	for i in imagenes_totales:
 		for f in imagenes_totales[i]:
-			var textureRect = TextureRect.new()
-			var imagen = load("res://Assets/ImagenesEntrenamiento/" + i + "/" + f)
-			textureRect.set_texture(imagen)
-			textureRect.rect_size = imagen.get_size()
-			textureRect.expand = true
-			textureRect.set_stretch_mode(5)
-			print("Antes: " + str(textureRect.rect_min_size))
-			textureRect.rect_min_size = textureRect.rect_size * Vector2(.008, .008)
-			print("Después: " + str(textureRect.rect_min_size))
-			grid.add_child(textureRect)
-		
+			var imagen = load("res://Escenas/Imagenes/" + i + "/" + f)
+			var img = imagen.instance()
+			img.set_position(posicion)
+			imagenes.append(img)
+			add_child(img)
+			posicion.x += 120
+			if posicion.x + 120 >= get_viewport().size.x:
+				posicion.x = 80
+				posicion.y += 100
+
+func _input(event):
+	if event is InputEventMouseButton:
+		for i in imagenes:
+			if event.position >= i.get_global_position() and event.position <= Vector2(i.get_global_position().x + 50, i.get_global_position().y + 40):
+				print(str(event.position) + str(i.get_global_position()))
 	
 func list_files_in_directory(path):
 	var files = []
