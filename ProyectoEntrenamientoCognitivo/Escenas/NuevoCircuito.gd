e02da63274a2808b2d1f9cf0f1ce73b232ad5f87
lautaropastorino <lautaro.pastorino@hotmail.com>
2020-07-29 14:09:55 -0300

    Layout de nuevo circuito arreglado y terminado

--- ProyectoEntrenamientoCognitivo/Escenas/NuevoCircuito.gd
+++ ProyectoEntrenamientoCognitivo/Escenas/NuevoCircuito.gd
@@ -6,6 +6,7 @@ var palabras = []
 var numeros = []
 var aritmeticos = []
 var imagenes = []
+onready var container = $Control/VBoxContainer
 
 func _ready():
 	#Obtengo todas las imagenes posibles
@@ -13,23 +14,32 @@ func _ready():
 	flechas = list_files_in_directory("res://Escenas/Imagenes/Flechas")
 	palabras = list_files_in_directory("res://Escenas/Imagenes/PalabrasEnColores")
 	numeros = list_files_in_directory("res://Escenas/Imagenes/Numeros")
+	numeros.sort_custom(self, "custom_array_sort");
 	aritmeticos = list_files_in_directory("res://Escenas/Imagenes/Aritmeticos")
 	
-	var imagenes_totales = {"Colores": colores, "Flechas": flechas, "Numeros": numeros}#, "PalabrasEnColores": palabras,  "Aritmeticos": aritmeticos}
+	var imagenes_totales = {"Colores": colores, "Flechas": flechas, "Numeros": numeros, "PalabrasEnColores": palabras,  "Aritmeticos": aritmeticos}
 	
 	var posicion = Vector2(80, 150)
 
 	for i in imagenes_totales:
+		var label = Label.new()
+		label.set_text(i + ": ")
+		container.add_child(label)
+		var scroll = ScrollContainer.new()
+		scroll.rect_min_size = Vector2(100,90)
+		var hBox = HBoxContainer.new()
+		scroll.add_child(hBox)
 		for f in imagenes_totales[i]:
 			var imagen = load("res://Escenas/Imagenes/" + i + "/" + f)
 			var img = imagen.instance()
-			img.set_position(posicion)
+			#img.set_position(posicion)
 			imagenes.append(img)
-			add_child(img)
+			hBox.add_child(img)
 			posicion.x += 120
 			if posicion.x + 120 >= get_viewport().size.x:
 				posicion.x = 80
 				posicion.y += 100
+		container.add_child(scroll)
 
 func _input(event):
 	if event is InputEventMouseButton:
@@ -50,3 +60,8 @@ func list_files_in_directory(path):
 		elif not ".import" in file:
 			files.append(file)
 	return files
+
+func custom_array_sort(a, b):
+	a = a.split(".")[0]
+	b = b.split(".")[0]
+	return int(a) < int(b)
